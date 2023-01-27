9b422d73ae0b0d993f81d272d15ebb163f795210
Emerson Batista <84859510+emersongg21@users.noreply.github.com>
2023-01-27 17:33:11 +0000

    Fixed bug in link click area

---
+++ script.js
@@ -0,0 +1,19 @@
+function showTT(params) {
+  document.getElementById("ttIcon").style.visibility = "visible";
+}
+
+function openInsta() {
+  window.open("https://www.instagram.com/emerson21.dev/");
+}
+
+function openTwitter() {
+  window.open("https://twitter.com/ermissu21");
+}
+
+function openLinkedin() {
+  window.open("https://www.linkedin.com/in/emerson-batista-935238260/");
+}
+
+function openGithub() {
+  window.open("https://github.com/emersongg21/");
+}
